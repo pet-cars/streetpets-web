041190d6346ddc7457165612197907415fce2369
Mateus Moraes <mateus890alves@gmail.com>
2023-12-11 00:14:05 -0300

    apresentação

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router} from "react-router-dom"
-import './styles/global.css'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./styles/global.css";
 
-import AllRoutes from './routes'
-import { AuthProvider } from './context/auth/AuthProvider'
+import AllRoutes from "./routes";
+// import { AuthProvider } from './context/auth/AuthProvider'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(
   <Router>
-    <AuthProvider>
-      <AllRoutes />
-    </AuthProvider>
+    {/* <AuthProvider> */}
+    <AllRoutes />
+    {/* </AuthProvider> */}
   </Router>
-)
+);
